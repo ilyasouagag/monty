7ca02af6be93af0bc3900e340570ad5f7d8010f6
ilyas <ilyasouagag2011@gmail.com>
2023-10-19 16:08:28 +0100

    last update

--- monty.h
+++ monty.h
@@ -47,7 +47,7 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
-void div(stack_t **stack, unsigned int line_number);
+void m_div(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
 int check_digit(char *string);
